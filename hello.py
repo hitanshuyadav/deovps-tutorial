efb4c520cffe37fb366ae416489970d45f112f67
hitanshuyadav <hitanshuy76@gmail.com>
2026-06-23 12:56:06 +0000

    again changed file

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("hello world kaise ho app sab")
+for i in range(0,100):
+    print(i)
+    if i == 99 :
+        print("it is finished")
+        
+    else :
+        print("everything is ok and fine to work with dont worry")
